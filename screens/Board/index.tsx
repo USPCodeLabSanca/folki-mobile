9ae75d5ab085eff556868fca753f4f783e68d531
Allan Silva <allanvss15@gmail.com>
2026-02-04 15:29:50 -0300

    feat: add initial structure for mural page

---
+++ screens/Board/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DefaultBackground from "../../components/DefaultBackground";
+import Title from "../../components/Title";
+import Paragraph from "../../components/Paragraph";
+
+const Board = () => {
+  return (
+      <DefaultBackground>
+        <Title>Mural</Title>
+        <Paragraph>Fale o que quiser para a USP</Paragraph>
+      </DefaultBackground>
+  );
+}
+
+export default Board;
